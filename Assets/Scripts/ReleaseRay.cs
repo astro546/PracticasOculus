6dd917ef33b73dba245ccc175aa549e3d6bbf874
astro546 <oloroco1@hotmail.com>
2024-05-15 16:05:40 -0600

    Bug del arma con Oculus corregido

--- Assets/Scripts/ReleaseRay.cs
+++ Assets/Scripts/ReleaseRay.cs
@@ -6,11 +6,7 @@ using UnityEngine.EventSystems;
 
 public class ReleaseRay : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
 
     // Update is called once per frame
     void Update()
--- Assets/Scripts/SwitchControl.cs
+++ Assets/Scripts/SwitchControl.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
-using UnityEditor.SceneManagement;
 using UnityEngine;
 using static CheckXRDisplay;
 
@@ -11,11 +10,13 @@ public class SwitchControl : MonoBehaviour
     PlayerMovement playerMov;
     PlayerCam playerCam;
     GameObject oculusController;
+    OVRRaycaster raycaster;
     CheckXRDisplay check;
     Canvas canvas;
     public GameObject oculusControl;
     public GameObject Gun;
     public GameObject VRHand;
+    public GameObject LaserPointer;
 
     // Start is called before the first frame update
     void Start()
@@ -24,27 +25,26 @@ public class SwitchControl : MonoBehaviour
         playerMov = GetComponent<PlayerMovement>();
         playerCam = player.transform.GetChild(2).GetComponent<PlayerCam>();
         oculusController = player.transform.GetChild(3).gameObject;
+        raycaster = player.transform.GetChild(4).GetComponent<OVRRaycaster>();
         check = new CheckXRDisplay();
         canvas = player.transform.GetChild(3).GetComponent<Canvas>();
 
         if (check.Awake()){
             playerMov.enabled = false;
             playerCam.enabled = false;
+            raycaster.enabled = true;
             oculusController.SetActive(true);
             canvas.transform.SetParent(oculusController.transform);
             Gun.transform.SetParent(VRHand.transform);
         } else {
             playerMov.enabled = true;
             playerCam.enabled = true;
+            raycaster.enabled = false;
             oculusController.SetActive(false);
+            LaserPointer.SetActive(false);
             oculusControl.transform.SetParent(playerCam.transform);  
         }
         Gun.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
